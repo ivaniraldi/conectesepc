98f45533ffb0cad2b6961b1883d0b42cd521ae69
Fer <vendas.atacado11@gmail.com>
2025-07-23 20:53:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,11 +1,22 @@
+import { ClientOnly } from "../client-only";
+
+const CurrentYear = () => {
+  return <span className="font-semibold">{new Date().getFullYear()}</span>;
+}
+
 export function Footer() {
   return (
     <footer className="border-t border-border/40">
       <div className="container mx-auto flex h-16 items-center justify-center px-4">
         <p className="text-sm text-muted-foreground">
-          &copy; {new Date().getFullYear()} CONECTE-SE REPRESENTAÇÕES. Todos os direitos reservados.
+          &copy;{" "}
+          <ClientOnly>
+            <CurrentYear />
+          </ClientOnly>{" "}
+          CONECTE-SE REPRESENTAÇÕES. Todos os direitos reservados.
         </p>
       </div>
     </footer>
   );
 }
+    
